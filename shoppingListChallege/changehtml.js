16537eb7265303b772c3c13ed95fa8a78562e572
jonsung89 <jonsung89@gmail.com>
2018-07-19 18:58:55 -0500

    updated changehtml.js

---
+++ shoppingListChallege/changehtml.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const createItem = function(item) {
+  return `
+    <li>
+      ${item.name}
+      <button class="delete">
+        <span>Delete</span>
+      </button>
+    </li>
+  `;
+};
+
+
+$(function() {
+
+  $('#add-item').on('click', () => )
+
+})
