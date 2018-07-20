5478813467a07b599fee6a1aeb1e5ad1f4d68a08
Jon Sung <jonsung89@gmail.com>
2018-07-20 01:04:09 -0500

    edited changehtml.js - adds user's input to the list when button is pressed

--- shoppingListChallenge/changehtml.js
+++ shoppingListChallenge/changehtml.js
@@ -1,30 +1,68 @@
 'use strict';
 
-
-const generateItemHtml = function(item) {
-  return `
-      <li>
-      ${item}
-      <button class="check">
-        check
-      </button>
-      <button class="delete">
-        delete
-      </button>
-    </li>
-    `
+const generateItemHtml = function() {
+  const userItem = $('.user-input').val();
+  if (!userItem) {
+    alert('Add an item!');
+  } else {
+    return `
+        <li>
+        ${userItem}
+        <button class="check">
+          check
+        </button>
+        <button class="delete">
+          delete
+        </button>
+      </li>
+      `
     ;
+  }
 };
-
 
 function renderItem() {
   $('.add-item-form').on('submit', function(event) {
     event.preventDefault();
     // use $(this) find jquery method event del<< and propagation
-    const myitem = generateItemHtml('broccoli');
-    console.log(myitem);
+
+    const myitem = generateItemHtml();
+
     $('.shop-list').append(myitem);
-  } )
+  });
 }
 
-$(renderItem)
+$(renderItem);
+
+
+
+
+
+// ***Previous Version***
+
+// const generateItemHtml = function(item) {
+//   return `
+//         <li>
+//         ${item}
+//         <button class="check">
+//           check
+//         </button>
+//         <button class="delete">
+//           delete
+//         </button>
+//       </li>
+//       `
+//     ;
+// };
+
+// function renderItem() {
+//   $('.add-item-form').on('submit', function(event) {
+//     event.preventDefault();
+//     // use $(this) find jquery method event del<< and propagation
+
+//     const myitem = generateItemHtml('broccoli');
+//     console.log(myitem);
+//     $('.shop-list').append(myitem);
+//   })
+// }
+
+// $(renderItem);
